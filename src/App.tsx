a9dcde56ea073df7adc6aeba32b45b3d8a52cce3
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-02 11:22:21 +0100

    feat(app.tsx and react router): add react router
    add react router + under construction logo temporarily added on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import UnderConstruction from './assets/images/underConstruction.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display : 'flex', flexDirection: 'column'}}>
+      <h1 style={{ textAlign: 'center', marginBottom: '32px' }} >Site under construction</h1>
+      <img src={UnderConstruction} style={{alignSelf : 'center'}} alt='site under construction'></img>
     </div>
   );
 }
