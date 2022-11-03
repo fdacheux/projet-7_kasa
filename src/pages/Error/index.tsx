c62e1fd5b515e110af8675a0c6df503e1818deb7
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-03 18:41:11 +0100

    refactor(all pages): add main tags on every pages

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -3,15 +3,19 @@ import style from "./Error.module.scss";
 
 function Error() {
   return (
-    <section className={style.errorWrapper}>
-      <div className={style.errorBlock}>
-        <h1 className={style.title}>404</h1>
-        <h2 className={style.errorMessage}>
-          Oups! La page que vous demandez n'existe pas.
-        </h2>
-      </div>
-      <Link to="/" className={style.homeLink}>Retourner sur la page d'accueil</Link>
-    </section>
+    <main>
+      <section className={style.errorWrapper}>
+        <div className={style.errorBlock}>
+          <h1 className={style.title}>404</h1>
+          <h2 className={style.errorMessage}>
+            Oups! La page que vous demandez n'existe pas.
+          </h2>
+        </div>
+        <Link to="/" className={style.homeLink}>
+          Retourner sur la page d'accueil
+        </Link>
+      </section>
+    </main>
   );
 }
 
