55be9b44397213d837f282ccadb40207174d251c
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-04 15:28:52 +0100

    feat(gallery and cards): add responsive
    add a responsive style on gallery and cards elements

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,8 +3,10 @@ import Img from "../../assets/images/Img.png";
 
 function Card() {
   return (
-    <article className={style.card}>
-      <img src={Img} alt="img" className={style.temporaryImg} />
+      <article className={style.card}>
+          <div className={style.imgBox}>
+              <img src={Img} alt="img" className={style.temporaryImg} />
+          </div>
       <h3 className={style.title}>Titre de location</h3>
     </article>
   );
