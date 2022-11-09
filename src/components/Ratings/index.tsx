26358645e8b5e8f4ed3ce59d8362366efb793795
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-09 15:14:50 +0100

    feat(rating component): add aria-label to stars container
    to improve accessibility, indicates in an aria-labe in stars container div element the rating for
    the flat

--- src/components/Ratings/index.tsx
+++ src/components/Ratings/index.tsx
@@ -1,27 +1,22 @@
 import FullStar from "../../assets/images/full-star.svg";
 import EmptyStar from "../../assets/images/empty-star.svg";
-import { useEffect } from "react";
 
 const Ratings = ({ stars }: any) => {
   const fullStars = parseInt(stars);
 
-  useEffect(() => console.log("stars?", stars));
-
   const renderStars = (count: number): React.ReactElement[] => {
     let starsElements: React.ReactElement[] = [];
-
-    console.log("count", count);
 
     for (let x = 0; x < 5; x++) {
       starsElements = [
         ...starsElements,
-        x < count ? <img src={FullStar} /> : <img src={EmptyStar}></img>,
+        x < count ? <img src={FullStar} alt={`étoile pleine ${x+1} sur 5`} /> : <img src={EmptyStar} alt="étoile vide"/>,
       ];
     }
 
     return starsElements;
   };
-  return <div>{stars && renderStars(fullStars)}</div>;
+  return <div aria-label={`Cette location a reçu une note de ${stars} sur 5`}>{stars && renderStars(fullStars)}</div>;
 };
 
 export default Ratings;
