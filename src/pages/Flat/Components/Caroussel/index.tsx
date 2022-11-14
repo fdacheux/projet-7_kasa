71ca8b4080a48711fda1d98f5334fcb296d2f8b2
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-14 14:36:06 +0100

    feat(carousel): add controls style
    add arrows images buttons and page progression

--- src/pages/Flat/Components/Caroussel/index.tsx
+++ src/pages/Flat/Components/Caroussel/index.tsx
@@ -1,17 +1,30 @@
 import { IFlat } from "../../../../models/flat.model";
 import style from "./Carousel.module.scss";
+import LeftChevron from "../../../../assets/images/left-arrow.svg";
+import RightChevron from "../../../../assets/images/right-arrow.svg";
 
 const Carousel = ({ pictures, title }: IFlat) => {
   return (
     <div className={style.carouselContainer}>
-      {pictures.map((picture: string) => 
-        (<img
+      {pictures.map((picture: string) => (
+        <img
           src={picture}
           alt={title}
           key={`carouselPicture-${picture}`}
           className={style.carouselPhotos}
-        />)
-      )}
+        />
+      ))}
+      <div className={style.carouselContainer__controls}>
+        <button aria-label="Image précédente" className={style.carouselContainer__leftChevron}>
+          <img src={LeftChevron} alt="" className={`${style.chevrons} ${style.leftChevron}`} />
+        </button>
+        <button aria-label="Image suivante" className={style.carouselContainer__rightChevron}>
+          <img src={RightChevron} alt="" className={`${style.chevrons} ${style.rightChevron}`}/>
+        </button>
+      </div>
+      <div className={style.carouselContainer__imagesProgression}>
+        <p>1/4</p>
+      </div>
     </div>
   );
 };
