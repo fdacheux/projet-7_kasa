374629023e8914f1e3aef45f8023b220a023a757
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-18 18:14:32 +0100

    refactor(pagination): create pageNumberButtons component
    create a component to lighten pagination by removing the part of the pagination logic relative to
    the page number buttons

--- src/pages/Home/Components/Pagination/Buttons/PageNumberButtons.tsx
+++ src/pages/Home/Components/Pagination/Buttons/PageNumberButtons.tsx
@@ -1,10 +1,21 @@
 
-const PageNumberButtons = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import style from "./PageNumberButtons.module.scss"
 
-export default PageNumberButtons
+const PageNumberButtons = (pageNumber: number, page: number) => {
+  return (
+    <div className={style.pageNumber}>
+    {[...Array(pageNumber)].map((x, number) => (
+      <button
+        key={`pagination-page${number + 1}of${pageNumber}`}
+        className={style.pageNumber__button}
+            // onClick={() => goToPage(number + 1)}
+            disabled={page === number+1}
+      >
+        {number + 1}
+      </button>
+    ))}
+  </div>
+  );
+};
+
+export default PageNumberButtons;
