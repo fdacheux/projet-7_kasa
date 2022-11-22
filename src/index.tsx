02ca1a919d820646fd851c3c99a99ee69ac3b7e2
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-11-22 23:06:23 +0100

    refactor(index): remove useless bit of code

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.scss";
 import "./style/base/reset.scss";
 import "./style/base/typography.scss";
 import Home from "./pages/Home";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Flat from "./pages/Flat";
 import About from "./pages/About";
@@ -30,8 +29,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
